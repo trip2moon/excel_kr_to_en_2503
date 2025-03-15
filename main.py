a143fb4032294658f696f1d8404a3ddc390bd369
Hyomoon Cho <hyomoon@hotmail.com>
2025-03-15 22:10:22 +0900

    code optimized
    code optimized

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import os
-import openpyxl
-from googletrans import Translator
-import json
 import asyncio
 import copy
+import json
+import os
+
+import openpyxl
+from googletrans import Translator
 
 DIR_EXCEL_INPUT = 'excel_input'
 DIR_EXCEL_OUTPUT = 'excel_output'
 DIR_REFERENCE = 'reference'
 DIR_EXCEL_TARGET = 'excel_target'
-
 
 FILE_EXCEL_EXT = 'xlsx'
 FILE_EXCEL_NAME = '엑셀_헌법_20250316'
@@ -24,6 +24,7 @@ FILE_REFERENCE_TARGET_JSON = f'reference_target_translated.json'
 
 FONT_NAME_EN = 'Consolas'
 
+
 def get_file_path_input_output():
     if os.path.exists(DIR_EXCEL_OUTPUT) is False:
         os.makedirs(DIR_EXCEL_OUTPUT)
@@ -31,11 +32,13 @@ def get_file_path_input_output():
     file_path_output = os.path.join(DIR_EXCEL_OUTPUT, FILE_EXCEL_OUTPUT)
     return file_path_input, file_path_output
 
+
 def get_file_path_reference():
     if os.path.exists(DIR_REFERENCE) is False:
         os.makedirs(DIR_REFERENCE)
     file_path = os.path.join(DIR_REFERENCE, FILE_REFERENCE_TARGET_JSON)
     return file_path
+
 
 def load_reference_target():
     reference_dict = dict()
@@ -48,10 +51,12 @@ def load_reference_target():
 
     return reference_dict
 
+
 def save_reference(reference_dict):
     file_path = get_file_path_reference()
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(reference_dict, f, indent=4, sort_keys=False, ensure_ascii=False)
+
 
 def check_reference(source_value, reference_dict):
     found = False
@@ -60,23 +65,16 @@ def check_reference(source_value, reference_dict):
         return found, reference_dict[source_value]
     return found, ''
 
-async def translate_ko_to_en(text, src='ko', dest='en'):
-    text_translated = ''
-    async with Translator() as translator:
-        result = await translator.translate(text, dest='en' )
-        text_translated = result
-        print(f"Output :{result.text}")
-    return text_translated
 
-def process_translation_by_dir(dir_excel_target, reference_dict):
-
+def process_translation_by_dir(dir_excel_target, cache_dict):
     file_list = sorted(os.listdir(dir_excel_target))
     for file_one in file_list:
         if file_one.endswith(FILE_EXCEL_EXT) is True:
             file_path = os.path.join(dir_excel_target, file_one)
-            asyncio.run(process_translation(file_path, reference_dict))
+            asyncio.run(process_translation(file_path, cache_dict))
 
-async def process_translation(file_path_input, reference_dict):
+
+async def process_translation(file_path_input, cache_dict):
     translator = Translator()
     wb = openpyxl.load_workbook(filename=file_path_input)
 
@@ -98,39 +96,31 @@ async def process_translation(file_path_input, reference_dict):
         data_dict = dict()
         ws_input = wb[sheet_name]
 
-
-        # ws_output = wb.create_sheet(sheet_name_new)
         ws_output = wb.copy_worksheet(ws_input)
         ws_output.title = sheet_name_new
-
-
 
         for row in ws_input.iter_rows(min_row=ws_input.min_row, max_row=ws_input.max_row):
             for cell in row:
                 row_index = cell.row
-                col_index = cell.column
                 col_letter = cell.column_letter
                 if cell.value is None:
                     continue
                 value_str = str(cell.value)
                 if value_str is not None:
                     cell_loc = f'{col_letter}{row_index}'
-                    # print(f"[{cell_loc}]-Original-{value_str}")
-                    reference_found, translated = check_reference(value_str, reference_dict)
+                    print(f"[{cell_loc}]-Original-{value_str}")
+                    reference_found, translated = check_reference(value_str, cache_dict)
                     if reference_found is False:
-
                         translated_result = await translator.translate(value_str, dest='en')
                         translated = translated_result.text
-                        reference_dict[value_str] = translated
-                        # print(f"[{cell_loc}]-Translated-{translated}")
-                    # else:
-                        # print(f"[{cell_loc}]-Existed-{translated}")
+                        cache_dict[value_str] = translated
+                        print(f"[{cell_loc}]-Translated-{translated}")
+                    else:
+                        print(f"[{cell_loc}]-Existed-{translated}")
 
                     data_dict[cell_loc] = translated
 
-
-                    new_cell = ws_output.cell(row=cell.row, column=cell.col_idx,
-                                          value=translated)
+                    new_cell = ws_output.cell(row=cell.row, column=cell.col_idx, value=translated)
                     if cell.has_style:
                         font_new = copy.copy(cell.font)
                         font_new.name = FONT_NAME_EN
@@ -141,7 +131,7 @@ async def process_translation(file_path_input, reference_dict):
                         new_cell.protection = copy.copy(cell.protection)
                         new_cell.alignment = copy.copy(cell.alignment)
 
-    save_reference(reference_dict)
+    save_reference(cache_dict)
 
     wb.save(file_path_input)
 
@@ -151,9 +141,3 @@ async def process_translation(file_path_input, reference_dict):
 if __name__ == '__main__':
     reference_dict = load_reference_target()
     process_translation_by_dir(DIR_EXCEL_TARGET, reference_dict)
-
-
-
-    # text_ex_01 = 'AI기술개발 지원 및 AI솔루션 확산을 위한 제조AI오픈랩 구축·운영 및 AI솔루션 도입 어려움 해소를 위한 AI솔루션 도입 컨설팅'
-    # print(f"Input :{text_ex_01}")
-    # asyncio.run(translate_ko_to_en(text_ex_01))
